aa5654cbf7823882e439cc87f22a201856dc4a84
Bowen Mao <b.mao@columbia.edu>
2018-02-23 16:24:51 -0500

    Skeleton code
    Skeleton code.

---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+import sys
+import os.path
+import main
+import unittest
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+
+class MainTest(unittest.TestCase):
+    """This class uses the Flask tests app to run an integration test against a
+    local instance of the server."""
+
+    def setUp(self):
+        self.app = main.app.test_client()
+
+    def test_hello_world(self):
+        rv = self.app.get('/')
+        data = rv.data.decode("utf-8")
+        print(data)
+        assert("hello" in data.lower())
+
+
+if __name__ == '__main__':
+    unittest.main()
