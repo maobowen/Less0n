c08da7811c533a5e0be1702e2ec91fed1a0174a4
Bowen Mao <b.mao@columbia.edu>
2018-03-16 13:07:21 -0400

    Implemented login with Lionmail
    Implemented login with Lionmail.

--- less0n/views.py
+++ less0n/views.py
@@ -1,11 +1,103 @@
-from less0n import app
+# Reference:
+# - http://bitwiser.in/2015/09/09/add-google-login-in-flask.html
+# - https://stackoverflow.com/questions/34235590/how-do-you-restrict-google-login-oauth2-to-emails-from-a-specific-google-apps
+
+from less0n import app, db
+from less0n.models import *
+import json
 import logging
+import re
+from flask import url_for, redirect, render_template, session, request, flash
+from flask_login import login_required, login_user, logout_user, current_user
+from requests_oauthlib import OAuth2Session
+from requests.exceptions import HTTPError
+from config import Auth
+
+
+def get_google_auth(state=None, token=None):
+    if token:
+        return OAuth2Session(Auth.CLIENT_ID, token=token)
+    if state:
+        return OAuth2Session(Auth.CLIENT_ID, state=state, redirect_uri=app.config['GOOGLE_OAUTH_REDIRECT_URI'])
+    oauth = OAuth2Session(Auth.CLIENT_ID, redirect_uri=app.config['GOOGLE_OAUTH_REDIRECT_URI'], scope=Auth.SCOPE)
+    return oauth
 
 
 @app.route('/')
-def hello():
+def index():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    session['oauth_redirect'] = request.args.get('redirect') or url_for('index')
+    if current_user.is_authenticated:
+        return redirect(session['oauth_redirect'])
+
+    google = get_google_auth()
+    auth_url, state = google.authorization_url(
+        Auth.AUTH_URI, access_type='offline', prompt='select_account')
+    session['oauth_state'] = state
+    return redirect(auth_url)
+
+
+@app.route('/oauth')
+def oauth2callback():
+    # Redirect user to home page if already logged in.
+    if current_user is not None and current_user.is_authenticated:
+        return redirect(session.get('oauth_redirect', url_for('index')))
+    if 'error' in request.args:
+        if request.args.get('error') == 'access_denied':
+            return 'You denied access.'
+        return 'Error encountered.'
+    if 'code' not in request.args and 'state' not in request.args:
+        return redirect(url_for('login'))
+    else:
+        # Execution reaches here when user has
+        # successfully authenticated our app.
+        google = get_google_auth(state=session['oauth_state'])
+        try:
+            token = google.fetch_token(
+                Auth.TOKEN_URI,
+                client_secret=Auth.CLIENT_SECRET,
+                authorization_response=request.url)
+        except HTTPError:
+            return 'HTTPError occurred.'
+        google = get_google_auth(token=token)
+        resp = google.get(Auth.USER_INFO)
+        if resp.status_code == 200:
+            user_data = resp.json()
+            email = user_data['email']
+            uni, hd = email.split('@')
+            if (hd != 'columbia.edu' and hd != 'barnard.edu') or not re.compile(r'([a-z]{2,3}\d{1,4})').match(uni):
+                flash('You cannot login using this email', 'error')
+                return redirect(session.get('oauth_redirect', url_for('index')))
+
+            user = User.query.filter_by(email=email).first()
+            if user is None:
+                user = User()
+                user.email = email
+            user.id = user_data['id']
+            user.name = user_data['name']
+            print(token)
+            user.tokens = json.dumps(token)
+            user.avatar = user_data['picture']
+            db.session.add(user)
+            db.session.commit()
+
+            login_user(user)
+            return redirect(session.get('oauth_redirect', url_for('index')))
+        return 'Could not fetch your information.'
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    redirect_url = request.args.get('redirect') or url_for('index')
+    logout_user()
+    session.clear()
+    return redirect(redirect_url)
 
 
 @app.errorhandler(500)
