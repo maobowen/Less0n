1ea14217fd9c78493e8354e5b84926ca66e55c4e
elenahoho <heyilan1995@gmail.com>
2018-03-30 23:20:17 -0400

    fix show

--- less0n/static/js/course-detail.js
+++ less0n/static/js/course-detail.js
@@ -1,29 +1,30 @@
-var professor_list = [{
-    "name": "All",
-    "pic": "../static/img/cu.jpg",
-    "rating": "40%",
-    "grade": "90%",
-    "workload": "20%",
-    "tag": ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
-}, {
-    "name": "Ewan Lowe",
-    "pic": "../static/img/el.jpg",
-    "rating": "50%",
-    "grade": "70%",
-    "workload": "80%",
-    "tag": ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10"],
-}, {
-    "name": "Big White",
-    "pic": "../static/img/c-logo.jpg",
-    "rating": "30%",
-    "grade": "20%",
-    "workload": "50%",
-    "tag": ["w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9", "w10"],
-}];
-
-var cur = 0;
-
 $(document).ready(function() {
+
+    var professor_list = [{
+        "name": "All",
+        "pic": "../static/img/cu.jpg",
+        "rating": "40%",
+        "grade": "90%",
+        "workload": "20%",
+        "tag": ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"],
+    }, {
+        "name": "Ewan Lowe",
+        "pic": "../static/img/el.jpg",
+        "rating": "50%",
+        "grade": "70%",
+        "workload": "80%",
+        "tag": ["g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9", "g10"],
+    }, {
+        "name": "Big White",
+        "pic": "../static/img/c-logo.jpg",
+        "rating": "30%",
+        "grade": "20%",
+        "workload": "50%",
+        "tag": ["w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9", "w10"],
+    }];
+
+    var cur = 0;
+
     $('#left i').click(function() {
         if (cur == -1) {
             cur = professor_list.length-1;
