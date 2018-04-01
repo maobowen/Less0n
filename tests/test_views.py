a16d6b2230aa7896b78cb77cfa7865656f409541
姜志健 <zhijian@jiangzhijiandeMacBook-Pro.local>
2018-04-01 13:22:24 -0400

    Complete the test of views.py 1. test /dept/../ 2. test /course/../ 3. test /course/../json/

--- tests/test_views.py
+++ tests/test_views.py
@@ -28,7 +28,7 @@ class MainTest(unittest.TestCase):
         assert(rv._status_code == 302)
         assert(Auth.AUTH_URI in rv.location)
 
-    def test_department_course_with_valid_input(self):
+    def test_department_course_with_valid_arg(self):
         """
         Test if department_course() return department-course.html with valid department name
         Test cases:
@@ -43,13 +43,13 @@ class MainTest(unittest.TestCase):
         assert 'department-course' in data
         assert 'coms' in data
 
-    def test_department_course_with_unvalid_input(self):
+    def test_department_course_with_unvalid_arg(self):
         """
         Test if department_course() return department-course.html with unvalid department name
         Test cases:
         --------------------------------------------------
         Input                             Expected Output
-        /dept/AAAA/                       department in html
+        /dept/AAAA/                       302, department in html
         """
         rv = self.app.get('/dept/AAAA/')
         assert rv._status_code == 302
@@ -57,8 +57,68 @@ class MainTest(unittest.TestCase):
         data = rv.data.decode('utf-8').lower() # convert data to lower case
         assert 'department' in data
 
-    def test_department_course_with_empty_input(self):
-        pass
+    def test_course_with_valid_arg(self):
+        """
+        Test if course() return course-detail.html with valid argument
+        Test case:
+        --------------------------------------------------
+        Input                             Expected Output
+        /course/COMS3157/                       department in html
+        """
+        rv = self.app.get('/course/COMS3157/')
+        assert rv._status_code == 200
+        assert rv.content_type == 'text/html; charset=utf-8'
+        data = rv.data.decode('utf-8').lower()  # convert data to lower case
+        assert 'course-detail' in data
+        assert 'coms3157' in data
+
+
+    def test_course_with_unvalid_arg(self):
+        """
+        Test if course() return course-detail.html with valid argument
+        Test case:
+        --------------------------------------------------
+        Input                             Expected Output
+        /course/COMS2222/                 department in html
+        /course/ABCDEFG/                  404
+        """
+        rv = self.app.get('/course/COMS2222/')
+        assert rv._status_code == 302
+        assert rv.content_type == 'text/html; charset=utf-8'
+        data = rv.data.decode('utf-8').lower()  # convert data to lower case
+        assert 'department' in data
+
+        rv = self.app.get('/course/ABCDEFG/')
+        assert rv._status_code == 404
+
+    def test_course_json_with_valid_arg(self):
+        """
+        Test if course() return course-detail.html with valid argument
+        Test case:
+        --------------------------------------------------
+        Input                             Expected Output
+        /course/COMS3157/json             JSON data
+        """
+        rv = self.app.get('/course/COMS3157/json/')
+        assert rv._status_code == 200
+        assert rv.content_type == 'application/json'
+
+    def test_course_json_with_unvalid_arg(self):
+        """
+        Test if course() return course-detail.html with valid argument
+        Test case:
+        --------------------------------------------------
+        Input                             Expected Output
+        /course/COMS1234/json             empty JSON data
+        /course/ABCDEFG/json              404
+        """
+        rv = self.app.get('/course/COMS1234/json/')
+        assert rv._status_code == 200
+        assert rv.content_type == 'application/json'
+        assert rv.data.decode('utf-8') == '[]\n'
+
+        rv = self.app.get('/course/ABCDEFG/json/')
+        assert rv._status_code == 404
 
 
 if __name__ == '__main__':
