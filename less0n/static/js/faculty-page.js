a9851df36c01507c6011ae462ca9ab2a2aac2d66
elenahoho <heyilan1995@gmail.com>
2018-04-07 18:36:24 -0400

    finish prof js

---
+++ less0n/static/js/faculty-page.js
@@ -0,0 +1,53 @@
+var color_pool = ['success', 'primary', 'info', 'danger', 'warning'];
+
+for (var i = 0; i < $('.tags a').length; i++) {
+    var color = i%5;
+    $('.tags a:nth-child('+(i+1)+')').addClass(color_pool[i]);
+}
+
+var rating = parseFloat(""+$('#rating_num').text());
+var grade = parseFloat(""+$('#grade_num').text());
+var workload = parseFloat(""+$('#workload_num').text());
+
+if ($('#rating_num').text() != 'N/A') {
+    $('#rating_progress_bar').css('width', rating / 5 * 100 + '%');
+    $('#rating_progress_bar').addClass(rating_to_color(rating));
+} else {
+    $('#rating_progress_bar').css('width', 0);
+}
+
+if ($('#grade_num').text() != 'N/A') {
+    $('#grade_progress_bar').css('width', grade / 4.33 * 100 + '%');
+    $('#grade_progress_bar').addClass(gpa_to_color(grade));
+} else {
+    $('#rating_progress_bar').css('width', 0);
+}
+
+if ($('#workload_num').text() != 'N/A') {
+    $('#workload_progress_bar').css('width', workload / 5 * 100 + '%');
+    $('#workload_progress_bar').addClass(workload_to_color(workload));
+} else {
+    $('#rating_progress_bar').css('width', 0);
+}
+
+$(".card").each(function(index) {
+    var ratingdiv = $(".row .col-md-6:nth-child("+(index+1)+") div:nth-child(5)");
+    var gradediv = $(".row .col-md-6:nth-child("+(index+1)+") div:nth-child(6)");
+    var workloaddiv = $(".row .col-md-6:nth-child("+(index+1)+") div:nth-child(7)");
+
+    var rating_num = ratingdiv.text().split(" ")[1];
+    var grade_num = gradediv.text().split(" ")[1];
+    var workload_num = workloaddiv.text().split(" ")[1];
+
+    var rating = parseFloat(rating_num);
+    ratingdiv.addClass(rating_to_color(rating));
+
+    var grade = parseFloat(grade_num);
+    gradediv.addClass(gpa_to_color(grade));
+
+    var workload = parseFloat(workload_num);
+    workloaddiv.addClass(workload_to_color(workload));
+
+    var border_div = $(".row .col-md-6:nth-child("+(index+1)+") .card .card-body");
+    border_div.addClass('border-'+rating_to_color(rating));
+});
